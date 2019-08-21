02bbf6fbd27c06225247c09da4ecfeb9839a4dbb
dillnorm <31057595+dillnorm@users.noreply.github.com>
2019-08-21 10:25:35 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,50 @@
+
+
 function() {
     this.res.writeHead(200);
     postMessage("Wassup test");
     this.res.end();
   } 
 function();
+
+function postMessage(response) {
+  var botResponse,options, body, botReq;
+
+  botResponse = response
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+exports.respond = respond;

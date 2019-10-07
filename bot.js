38c9dfe7b32e777de761d75c2abe135c71d9e861
dillnorm <31057595+dillnorm@users.noreply.github.com>
2019-10-07 13:21:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,7 +89,7 @@ function ptDay(){
     else if(d1.getDate() == 13){
         rep = "FOR MONDAY\n What: \n \n Preparation \n PD (5 reps)\n Activities\n BODY WEIGHT CIRCUIT\n Cadets conduct circuits consisting of 25\n push-ups, 30 sit-ups, 10 Leg tucks, 20\n Lunges, and 20 air squats with 1 lap between circuits. Cadets will complete\n5 circuits.\n \n Recovery\n RD (30 sec) \n \n Who: \n Primary Leader\n Grizzard\n Secondary Leader\n King\n \n Where: \n RAC PAVILION\n \n When:\n 0615 Formation\n 0630-0730 PT        } 
     /*
-      else if(d1.getDate() == 14){
+    else if(d1.getDate() == 14){
         rep = "FOR WEDNESDAY\nWhat: \n\n Ruck March\n6MI\n APFU’s\n45 lbs.\n\n Who: \n HC Section 1\n\nWhere: \nRAC Pavilion\n\nWhen:  \n0615 Formation\n0630-0730 PT\n\nWhat: \n\nRuck March\n6MI\nAPFU’s\n45 lbs.\n\nWho: \nMSIII’s & HHC Section 2\n\nWhere: \nGentilly Trail\n\nWhen:  \n0515 Formation\n0530-0730 PT"; } 
     else if(d1.getDate() == 15){
         rep = "FOR WEDNESDAY\nWhat: \n\n Ruck March\n6MI\n APFU’s\n45 lbs.\n\n Who: \n HHC Section 1\n\nWhere: \nRAC Pavilion\n\nWhen:  \n0615 Formation\n0630-0730 PT\n\nWhat: \n\nRuck March\n6MI\nAPFU’s\n45 lbs.\n\nWho: \nMSIII’s & HHC Section 2\n\nWhere: \nGentilly Trail\n\nWhen:  \n0515 Formation\n0530-0730 PT"; } 
